959d139ac0c08422369abe315d2c80c106a80f92
DeepakThiagarajan <deepak.badri1105@gmail.com>
2023-02-10 15:14:47 +0530

    5th commit

---
+++ sample4.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+int main()
+{
+
+/*int i=1011;
+float f=11.11;
+double d=12.22;
+char c='x';
+
+unsigned int ui=100;
+
+printf("\ni=%03d",i);
+printf("\nf=%f",f);
+printf("\nd=%lf",d);
+printf("\nui=%u",&i);
+printf("\n\n");*/
+
+int i;
+float f;
+double d;
+char c;
+
+scanf("%c",&c);
+scanf("%d",&i);
+
+printf(" \n i=%d",i);
+printf(" \n c=%c Ascii value=%d",c,c);
+return 0;
+}
