e673c2b120b899e215942d2beaa236564b5f84b6
DeepakThiagarajan <118879597+DeepakThiagarajan@users.noreply.github.com>
2023-03-30 23:32:13 +0530

    Update structure.h

--- Project/Local_Search_Engine/inc/structure.h
+++ Project/Local_Search_Engine/inc/structure.h
@@ -3,16 +3,6 @@
 
 #include "headers.h"
 
-/************************************************************************************
-**     STRUCTURE NAME     :     File
-**
-**    DESCRIPTION        :      structure to create node for the File Linked List  
-**
-**     RETURNS           :      NOT APPLIED
-**
-**
-************************************************************************************/
-
 typedef struct struct_File{
     char f_name[MAX_LENGTH];
     char f_path[MAX_PATH_LENGTH];
