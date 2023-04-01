949317be3c7e4e446da81aaff1ab5502876cd4c3
DeepakThiagarajan <118879597+DeepakThiagarajan@users.noreply.github.com>
2023-04-01 22:12:39 +0530

    Update server.c

--- server.c
+++ server.c
@@ -6,116 +6,103 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <unistd.h>
-#include <dirent.h> // For directory operations
-#include <errno.h> // For error handling
 
 #define MAXBUFF 1024
 #define PORTNO 4003
 
-
 int main()
 {
-	int sfd = 0, retValue=0, csfd=0;
-	int clientAddlen = 0;
+    int sfd = 0, retValue = 0, csfd = 0;
+    int clientAddlen = 0;
 
-	struct sockaddr_in serv_address, client_address;
+    struct sockaddr_in serv_address, client_address;
 
-	char msg[MAXBUFF] = {0,};
-	char filename[MAXBUFF] = {0,};
+    char msg[MAXBUFF] = {0,};
+    char path[MAXBUFF] = {0,};
+    char command[MAXBUFF];
 
-	sfd = socket(AF_INET,SOCK_STREAM,0);
+    sfd = socket(AF_INET, SOCK_STREAM, 0);
 
-	if(sfd < 0)
-	{
-		perror("socket() ");
-		exit(EXIT_FAILURE);
-	}
-	printf("\nSocket created with sockfd : %d",sfd);
+    if (sfd < 0)
+    {
+        perror("socket() ");
+        exit(EXIT_FAILURE);
+    }
+    printf("\nSocket created with sockfd : %d", sfd);
 
-	//reset/set address of server 
+    // reset/set address of server 
 
-	memset(&serv_address,'\0',sizeof(serv_address));
+    memset(&serv_address, '\0', sizeof(serv_address));
 
-	serv_address.sin_family = AF_INET;
-	serv_address.sin_port = htons(PORTNO);
-	serv_address.sin_addr.s_addr = inet_addr("127.0.0.1");
+    serv_address.sin_family = AF_INET;
+    serv_address.sin_port = htons(PORTNO);
+    serv_address.sin_addr.s_addr = INADDR_ANY;
 
-	retValue = bind(sfd, (struct sockaddr *)&serv_address,sizeof(serv_address));
-	if(retValue < 0)
-	{
-		perror("bind()");
-		exit(EXIT_FAILURE);
-	}
+    retValue = bind(sfd, (struct sockaddr *)&serv_address, sizeof(serv_address));
+    if (retValue < 0)
+    {
+        perror("bind()");
+        exit(EXIT_FAILURE);
+    }
 
-	printf("\nBinded the server to the ipaddress with portno\n");
+    printf("\nBinded the server to the ipaddress with portno\n");
 
-	retValue = listen(sfd, 5);
-	if(retValue < 0)
-	{
-		perror("listen() ");
-		exit(EXIT_FAILURE);
-	}
-	printf("\nListening to the clients now\n");
+    retValue = listen(sfd, 5);
+    if (retValue < 0)
+    {
+        perror("listen() ");
+        exit(EXIT_FAILURE);
+    }
+    printf("\nListening to the clients now\n");
 
-	while(1){
-		csfd = accept(sfd,(struct sockaddr *)&client_address,&clientAddlen);
+    while (1)
+    {
+        csfd = accept(sfd, (struct sockaddr *)&client_address, &clientAddlen);
 
-		if(csfd < 0)
-		{
-			perror("accept() ");
-			exit(EXIT_FAILURE);
-		}
-		printf("\nServer: Client got a connection\n");
+        if (csfd < 0)
+        {
+            perror("accept() ");
+            exit(EXIT_FAILURE);
+        }
+        printf("\nServer: Client got a connection\n");
 
-		strcpy(msg,"====================================================\n");
-		strcat(msg,"\n=============Welcome to Capgemini Server=============\n");
-		strcat(msg,"====================================================\n");
+        strcpy(msg, "====================================================\n");
+        strcat(msg, "\n=============Welcome to Capgemini Server=============\n");
+        strcat(msg, "====================================================\n");
 
-		write(csfd,msg,strlen(msg));
-		while(1)
-		{
-			memset(msg,'\0',MAXBUFF);
-			read(csfd,msg,MAXBUFF);
-			printf("Server: Client sent a msg: %s\n",msg);
-			
-			if (strncmp(msg, "search ", 7) == 0) {
-				strcpy(filename, msg+7); // Extract the filename from the message
+        write(csfd, msg, strlen(msg));
 
-				DIR *dir;
-				struct dirent *entry;
-				int file_found = 0;
-				
-				dir = opendir(".");
-				if (dir == NULL) {
-					perror("opendir");
-					exit(EXIT_FAILURE);
-				}
+        memset(msg, '\0', MAXBUFF);
+        read(csfd, msg, MAXBUFF);
+        read(csfd, path, MAXBUFF);
+        sprintf(command, "grep -r --include=*.{txt,c} '%s' '%s'", msg, path);
+        FILE *fp = popen(command, "r");
+        if (fp == NULL)
+        {
+            perror("popen()");
+            exit(EXIT_FAILURE);
+        }
+        memset(msg, '\0', MAXBUFF);
+        int i = 0;
+        while (fgets(msg, MAXBUFF, fp) != NULL)
+        {
+            char content[MAXBUFF];
+            sprintf(content, "%d--%s", i++, msg);
 
-				while ((entry = readdir(dir)) != NULL) {
-					if (strcmp(entry->d_name, filename) == 0) {
-						file_found = 1;
-						break;
-					}
-				}
+            int n = write(csfd, content, strlen(content));
+            if (n < 0)
+            {
+                perror("ERROR WRITING TO SOCKET");
+                exit(EXIT_FAILURE);
+            }
+            memset(msg, '\0', MAXBUFF);
+        }
+        pclose(fp);
+        close(csfd);
 
-				if (file_found) {
-					strcpy(msg, "file found\n");
-				} else {
-					strcpy(msg, "file not found\n");
-				}
+        printf("Server: Client sent a msg: %s\n", msg);
+    }
+    close(sfd);
 
-				closedir(dir);
-			}
-
-			write(csfd, msg, strlen(msg));
-			if(strcmp(msg,"bye\n")==0)
-			{
-				close(csfd);
-				break;
-			}
-		}
-	}
-	// close(sfd);
-
-	return 0;
+    return 0;
 }
