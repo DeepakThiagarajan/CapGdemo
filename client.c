b543b2d72a663168fc39b73cebb76a1023384b92
DeepakThiagarajan <118879597+DeepakThiagarajan@users.noreply.github.com>
2023-04-01 22:11:24 +0530

    Update client.c

--- client.c
+++ client.c
@@ -8,14 +8,22 @@
 #include <unistd.h>
 
 #define MAXBUFF 1024
-#define PORTNO 4003
+#define PORTNO 7799
+#define MAXLEN 50
 
 int main()
 {
-    int sfd = 0, retValue = 0;
+    int sfd = 0, retValue = 0, c;
     int clientAddlen = 0;
     struct sockaddr_in serv_address;
-    char msg[MAXBUFF] = {0,};
+    char msg[MAXBUFF] = {0};
+    char path[MAXBUFF] = {0};
+    char *username = getlogin();
+    strcat(path, "/home2/");
+    strcat(path, username);
+    strcat(path, "/");
+    
+    int search = 0;
 
     sfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sfd < 0) {
@@ -25,7 +33,7 @@ int main()
     printf("\nSocket created with sockfd : %d", sfd);
 
     // Reset/set address of server 
-    memset(&serv_address, '\0', sizeof(serv_address));
+    memset(&serv_address, 0, sizeof(serv_address));
     serv_address.sin_family = AF_INET;
     serv_address.sin_port = htons(PORTNO);
     serv_address.sin_addr.s_addr = inet_addr("127.0.0.1");
@@ -38,11 +46,36 @@ int main()
     printf("\nClient: Connected to the server\n");
 
     // Request the server to search for a file
-    strcpy(msg, "search file1.txt");
+    printf("\nEnter the word/char to search \n");
+    scanf("%s", msg);
     write(sfd, msg, strlen(msg));
-
-    // Read the response from the server
-    memset(msg, '\0', MAXBUFF);
+    
+    printf("Any specific path(y for 1/n for 2) \n");
+    scanf("%d", &c);
+    char userpath[MAXLEN];
+    
+    switch(c)
+    {
+        case 1:
+            printf("Enter the path\n");
+            printf("%s", path);
+            fgets(userpath, MAXLEN, stdin);
+            userpath[strlen(userpath)-1] = '\0';
+            strcat(path, userpath);
+            strcat(path, "/");
+            printf("Searching with abspath ..\n");
+            write(sfd, path, 1000);
+            break;
+        case 2:
+            printf("Searching without abspath ..\n");
+            printf("%s", path);
+            write(sfd, path, 1000);
+            break;
+        default:
+            fprintf(stderr, "Invalid Choice !!\n");
+            break;
+    }
+    memset(msg, 0, MAXBUFF);
     read(sfd, msg, MAXBUFF);
     printf("%s", msg);
 
